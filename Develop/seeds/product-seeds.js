e313337d4bf8ba4e447d329945753e4bbc7c1cdb
Wolffkran <liebherr2008@live.com>
2024-01-17 18:42:05 -0500

    changed prices and stock numbers in product-seed

--- Develop/seeds/product-seeds.js
+++ Develop/seeds/product-seeds.js
@@ -3,32 +3,32 @@ const { Product } = require('../models');
 const productData = [
   {
     product_name: 'Classic White Tee',
-    price: 14.99,
-    stock: 14,
+    price: 19.99,
+    stock: 20,
     category_id: 1,
   },
   {
     product_name: 'Running Shoes for All',
-    price: 90.0,
-    stock: 25,
+    price: 100.0,
+    stock: 30,
     category_id: 5,
   },
   {
     product_name: 'Signature Logo Cap',
-    price: 22.99,
-    stock: 12,
+    price: 24.99,
+    stock: 15,
     category_id: 4,
   },
   {
     product_name: 'Golden Era Vinyl Collection',
-    price: 12.99,
-    stock: 50,
+    price: 15.99,
+    stock: 60,
     category_id: 3,
   },
   {
     product_name: 'Adventure Cargo Shorts',
-    price: 29.99,
-    stock: 22,
+    price: 34.99,
+    stock: 25,
     category_id: 2,
   },
 ];
